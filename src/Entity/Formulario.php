be7ddedb0147cbfc518c38ef97999dd28b116a93
Silzhar <luis.ruizfer@gmail.com>
2020-02-17 18:20:08 +0100

    maleteo / src

---
+++ src/Entity/Formulario.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormularioRepository")
+ */
+class Formulario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $usuario;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $localidad;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $opinion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsuario(): ?string
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario(string $usuario): self
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getLocalidad(): ?string
+    {
+        return $this->localidad;
+    }
+
+    public function setLocalidad(string $localidad): self
+    {
+        $this->localidad = $localidad;
+
+        return $this;
+    }
+
+    public function getOpinion(): ?string
+    {
+        return $this->opinion;
+    }
+
+    public function setOpinion(?string $opinion): self
+    {
+        $this->opinion = $opinion;
+
+        return $this;
+    }
+}
